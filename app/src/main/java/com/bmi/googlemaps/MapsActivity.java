39bf692dcdeb5d4eac44f76bd12f5285894d6045
Rohit Shrestha <rohit.xresta@gmail.com>
2020-01-03 18:47:05 +0545

    Search Map

--- app/src/main/java/com/bmi/googlemaps/MapsActivity.java
+++ app/src/main/java/com/bmi/googlemaps/MapsActivity.java
@@ -4,12 +4,17 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.bmi.googlemaps.model.LatitudeLongtitude;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,9 +44,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        List<LatitudeLongtitude>latlngs=new ArrayList<>();
+        latlngs.add(new LatitudeLongtitude(27.706195,85.3300396,"Softwarica"));
+        latlngs.add(new LatitudeLongtitude(27.7079284,85.330543,"Galaxy Public School"));
+
+        CameraUpdate center,zoom;
+        for (int i=0;i<latlngs.size();i++){
+            center=CameraUpdateFactory.newLatLng(new LatLng(latlngs.get(i).getLat(),latlngs.get(i).getLon()));
+            zoom=CameraUpdateFactory.zoomTo(16);
+            mMap.addMarker(new MarkerOptions().position(new LatLng(latlngs.get(i).getLat(),latlngs.get(i).getLon()))
+                    .title(latlngs.get(i).getMarker()));
+            mMap.moveCamera(center);
+            mMap.animateCamera(zoom);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+        }
     }
 }
